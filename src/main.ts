af8ce36119f41ba82263069e3d00adf771198a22
walttinho <walter.netto@live.com>
2024-04-29 00:00:20 -0300

    feat(app): Integrate Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Online Course Registration System')
+    .setDescription(
+      'API for managing online courses, with functionalities for teachers and students.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('auth')
+    .addTag('users')
+    .addTag('courses')
+    .addTag('lessons')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
