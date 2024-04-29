4b8d0e8a2150a7cfb27cebce862912cfe480d28d
Walttinho <walter.netto@live.com>
2024-04-29 12:38:15 -0300

    feat(env):  create .env exemple

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
